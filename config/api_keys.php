b536e8a2d3c1832795c2581f8aeb3d87015093e3
David Valdez <davidvaldez050708@gmail.com>
2026-08-31 08:45:44 -0600

    Actualiza modulo de informacion territorial

---
+++ config/api_keys.php
@@ -0,0 +1,18 @@
+<?php
+
+define(
+    'INEGI_INDICADORES_TOKEN',
+    '2ddbcd05-fa94-42be-85c0-90e048678818'
+);
+
+define(
+    'INEGI_INDICADORES_BASE_URL',
+    'https://www.inegi.org.mx/app/api/indicadores/desarrolladores/jsonxml'
+);
+
+define('DENUE_TOKEN', '2ddbcd05-fa94-42be-85c0-90e048678818');
+
+define(
+    'DENUE_BASE_URL',
+    'https://www.inegi.org.mx/app/api/denue/v1/consulta'
+);
