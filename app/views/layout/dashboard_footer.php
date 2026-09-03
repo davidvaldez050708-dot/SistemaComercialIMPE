8a51f4a5e769b7ec57f8cb36736934f073be1e26
davidvaldez050708-dot <davidvaldez050708@gmail.com>
2026-09-03 14:39:22 -0600

    Carga vista previa de oficios en el dashboard

--- app/views/layout/dashboard_footer.php
+++ app/views/layout/dashboard_footer.php
@@ -174,6 +174,10 @@
 </script>
 
 <script
+    src="<?= BASE_URL ?>public/javascript/oficios_vista_previa.js?v=<?= filemtime(ROOT_PATH . '/public/javascript/oficios_vista_previa.js') ?>">
+</script>
+
+<script
     src="<?= BASE_URL ?>public/javascript/recordatorios.js?v=<?= filemtime(ROOT_PATH . '/public/javascript/recordatorios.js') ?>">
 </script>
 
