bfd42d0103a36ca17411c6fd4f9a78601596dfba
davidvaldez050708-dot <davidvaldez050708@gmail.com>
2026-09-07 09:04:54 -0600

    Carga refinamientos visuales del expediente

--- app/views/layout/dashboard_head.php
+++ app/views/layout/dashboard_head.php
@@ -77,6 +77,10 @@ $fotoPerfilUrl = $fotoPerfil !== ''
 
     <link
         rel="stylesheet"
+        href="<?= BASE_URL ?>public/css/seguimiento_expediente_v2.css?v=<?= filemtime(ROOT_PATH . '/public/css/seguimiento_expediente_v2.css') ?>">
+
+    <link
+        rel="stylesheet"
         href="<?= BASE_URL ?>public/css/seguimiento_flujo.css?v=<?= filemtime(ROOT_PATH . '/public/css/seguimiento_flujo.css') ?>">
 
     <link
