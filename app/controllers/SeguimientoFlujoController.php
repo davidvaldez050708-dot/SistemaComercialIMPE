29ad6fe2918be6b30220b2108fce14a79de095b8
davidvaldez050708-dot <davidvaldez050708@gmail.com>
2026-09-23 13:30:03 -0600

    Recibe adjuntos en correo de seguimiento

--- app/controllers/SeguimientoFlujoController.php
+++ app/controllers/SeguimientoFlujoController.php
@@ -336,7 +336,9 @@ class SeguimientoFlujoController
                 $seguimientoId,
                 $usuarioId,
                 $_POST['asunto'] ?? '',
-                $_POST['cuerpo'] ?? ''
+                $_POST['cuerpo'] ?? '',
+                $_POST['adjuntos_expediente'] ?? [],
+                $_FILES['adjuntos_nuevos'] ?? null
             );
             $codigoHttp = (int)($resultado['codigo_http'] ?? 200);
             unset($resultado['codigo_http']);
